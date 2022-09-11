88ed821c079fc93f448886b6d30a27902698c108
Nagaraj N <nnagarajece31@gmail.com>
2022-09-11 15:59:34 +0530

    tech-stack

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('.container').css({ 'width': ($('body').width() - 100) + "px" });
+    $('.container').css({ 'height': ($(window).height() - 100) + "px" });
+    animateDiv($('.a'));
+    animateDiv($('.b'));
+    animateDiv($('.c'));
+    animateDiv($('.d'));
+    animateDiv($('.e'));
+    animateDiv($('.f'));
+    animateDiv($('.g'));
+    animateDiv($('.h'));
+    animateDiv($('.i'));
+    animateDiv($('.j'));
+    animateDiv($('.k'));
+    animateDiv($('.l'));
+    animateDiv($('.m'));
+    animateDiv($('.n'));
+    animateDiv($('.o'));
+});
+
+function makeNewPosition($container) {
+    var h = $container.height() - 50;
+    var w = $container.width() - 50;
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    return [nh, nw];
+}
+
+function animateDiv($target) {
+    var newq = makeNewPosition($target.parent());
+    var oldq = $target.offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    $target.animate({
+        top: newq[0],
+        left: newq[1]
+    }, speed, function () {
+        animateDiv($target);
+    });
+};
+
+function calcSpeed(prev, next) {
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    var greatest = x > y ? x : y;
+    var speedModifier = 0.1;
+    var speed = Math.ceil(greatest / speedModifier);
+    return speed;
+}
